99653963e92accffce92899a4893c5c980346724
JackWeidman <jackweidman1@gmail.com>
2025-02-13 14:05:23 -0600

    fixing some styling and views stuff

--- congress/views.py
+++ congress/views.py
@@ -5,7 +5,11 @@ def congress(request):
     return render(request, "congress/congress.html")
 
 def house_not_home(request):
+    if request.headers.get("HX-Request"):
+        return render(request, "congress/partials/house_partial.html")
     return render(request, "congress/house.html")
 
 def i_am_the_senate(request):
+    if request.headers.get("HX-Request"):
+        return render(request, "congress/partials/senate_partial.html")
     return render(request, "congress/senate.html")
