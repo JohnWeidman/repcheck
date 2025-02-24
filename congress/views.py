d0f23259f5ba0a0a98349f7a37cf9c28eebd9a05
JackWeidman <jackweidman1@gmail.com>
2025-02-23 19:24:57 -0600

    Major changes to styling

--- congress/views.py
+++ congress/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Congress
 
 # Create your views here.
 def congress(request):
-    return render(request, "congress/congress.html")
+    congresses = Congress.objects.all()
+    return render(request, "congress/congress.html", {"congresses": congresses})
 
 def house_not_home(request):
+    congress_id = request.GET.get("congress")
+    congress = get_object_or_404(Congress, id=congress_id) if congress_id else None
+    context={"congress_number": congress.congress_number if congress else "Unknown"}
     if request.headers.get("HX-Request"):
-        return render(request, "congress/partials/house_partial.html")
-    return render(request, "congress/house.html")
+        return render(request, "congress/partials/house_partial.html", context)
+    return render(request, "congress/house.html", context)
 
 def i_am_the_senate(request):
+    congress_id = request.GET.get("congress")
+    congress = get_object_or_404(Congress, id=congress_id) if congress_id else None
+    context={"congress_number": congress.congress_number if congress else "Unknown"}
     if request.headers.get("HX-Request"):
-        return render(request, "congress/partials/senate_partial.html")
-    return render(request, "congress/senate.html")
+        return render(request, "congress/partials/senate_partial.html", context)
+    return render(request, "congress/senate.html", context)
