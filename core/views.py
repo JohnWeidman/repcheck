f4fc13c7187d774289a1f43cb92a69ea5346ae0a
JackWeidman <jackweidman1@gmail.com>
2025-06-13 18:46:27 -0500

    Improvments to landing page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Create your views here.
 
+API_KEY = os.getenv("CONGRESS_API_KEY")
+BASE_URL = "https://api.congress.gov/v3"
+
 def home(request):
-    return render(request, "core/home.html")
+    url = f"{BASE_URL}/bill?api_key={API_KEY}&limit=10"
+    response = requests.get(url)
+    if response.status_code == 200:
+        bills = response.json().get("bills", [])
+    else:
+        bills = []
+        
+    context = {
+        "bills": bills,
+    }
+    return render(request, "core/home.html", context)
