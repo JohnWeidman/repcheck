c11369cc48b0964a724d86e7196bb64451dd5d2d
JackWeidman <jackweidman1@gmail.com>
2025-06-22 19:06:23 -0500

    Member Cards instead of table

--- congress/views.py
+++ congress/views.py
@@ -27,14 +27,15 @@ def house_not_home(request):
                 membership__congress=congress_id, membership__chamber=chamber
             )
             .distinct()
-            .annotate(party=Subquery(membership_qs.values("party")[:1]))
+            .annotate(party=Subquery(membership_qs.values("party")[:1]), 
+                      district=Subquery(membership_qs.values("district")[:1]))
             .order_by("state", "name")
         )
 
     else:
         house_members = Member.objects.none()
 
-    p = Paginator(house_members, 10)
+    p = Paginator(house_members, 12)
     try:
         members_page = p.page(page)
     except PageNotAnInteger:
