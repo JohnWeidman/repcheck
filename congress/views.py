45ba62dc47251abae4103af9cea2da4a61bf7362
JackWeidman <jackweidman1@gmail.com>
2025-06-22 19:26:31 -0500

    Member cards in senate

--- congress/views.py
+++ congress/views.py
@@ -28,7 +28,8 @@ def house_not_home(request):
             )
             .distinct()
             .annotate(party=Subquery(membership_qs.values("party")[:1]), 
-                      district=Subquery(membership_qs.values("district")[:1]))
+                      district=Subquery(membership_qs.values("district")[:1]),
+                      leadership_role=Subquery(membership_qs.values("leadership_role")[:1]))
             .order_by("state", "name")
         )
 
@@ -74,11 +75,13 @@ def i_am_the_senate(request):
                 membership__congress=congress_id, membership__chamber=chamber
             )
             .distinct()
-            .annotate(party=Subquery(membership_qs.values("party")[:1]))
+            .annotate(party=Subquery(membership_qs.values("party")[:1]), 
+                      district=Subquery(membership_qs.values("district")[:1]),
+                      leadership_role=Subquery(membership_qs.values("leadership_role")[:1]))
             .order_by("state", "name")
         )
 
-    p = Paginator(senate_members, 10)
+    p = Paginator(senate_members, 12)
     try:
         members_page = p.page(page)
     except PageNotAnInteger:
