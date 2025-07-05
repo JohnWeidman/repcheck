18a411277654d2a565a7227ac2abe306313a6ec8
JohnWeidman <jackweidman1@gmail.com>
2025-07-05 05:29:18 -0500

    Blocked by Auth and Redis

--- citizens/views.py
+++ citizens/views.py
@@ -9,10 +9,10 @@ def citizen_home(request):
     client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+        model="gemini-2.0-flash", contents="Explain the concept of democracy in simple terms for an 8th grader."
     )
-    print(response.text)
-    return render(request, "citizens/index.html")
+    
+    return render(request, "citizens/index.html", {"response": response.text})
 
 def citizen_resources(request):
     return render(request, "citizens/citizen_resources.html")
