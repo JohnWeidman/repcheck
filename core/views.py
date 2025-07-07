296b83e54ffbfa691595292863e9fcdf309897b6
JohnWeidman <jackweidman1@gmail.com>
2025-07-07 16:30:31 -0500

    Continuing to work on caching

--- core/views.py
+++ core/views.py
@@ -10,19 +10,19 @@ load_dotenv()
 
 API_KEY = os.getenv("CONGRESS_API_KEY")
 BASE_URL = "https://api.congress.gov/v3"
+CACHE_TIMEOUT = 60 * 10  # 15 minutes
 
-@cache_page(10)  # Cache for 15 minutes
+
+@cache_page(CACHE_TIMEOUT)
 def home(request):
-    current_time = time.strftime("%Y-%m-%d %H:%M: %S")
     url = f"{BASE_URL}/bill?api_key={API_KEY}&limit=12"
     response = requests.get(url)
     if response.status_code == 200:
         bills = response.json().get("bills", [])
     else:
         bills = []
-        
+
     context = {
         "bills": bills,
-        "current_time": current_time,
     }
     return render(request, "core/home.html", context)
