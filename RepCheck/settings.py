ffaba53ab86c167b4f9251a7fd6d193375b55f21
JohnWeidman <jackweidman1@gmail.com>
2025-07-17 16:08:32 -0500

    settings.py update

--- RepCheck/settings.py
+++ RepCheck/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from shutil import which
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import secrets
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    default=secrets.token_urlsafe(nbytes=64),
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
