4dbe3152d1b1501abbf20f2161b5c1e5d4dd1119
JohnWeidman <jackweidman1@gmail.com>
2025-08-02 09:19:49 -0500

    Adding config=english back and removing duplicate postgres app declaration

--- RepCheck/settings.py
+++ RepCheck/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.postgres',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'django.core.cache',
