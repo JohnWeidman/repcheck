1d8a5b3c9d7fbaad629332a822fa4efe45e1c85f
JohnWeidman <jackweidman1@gmail.com>
2025-08-23 11:06:36 -0500

    adding try/except blocks to external api calls

--- core/views.py
+++ core/views.py
@@ -16,12 +16,14 @@ CACHE_TIMEOUT = 60 * 10  # 15 minutes
 @cache_page(CACHE_TIMEOUT)
 def home(request):
     url = f"{BASE_URL}/bill?api_key={API_KEY}&limit=12"
-    response = requests.get(url)
-    if response.status_code == 200:
-        bills = response.json().get("bills", [])
-    else:
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            bills = response.json().get("bills", [])
+        else:
+            bills = []
+    except requests.RequestException:
         bills = []
-
     context = {
         "bills": bills,
     }
